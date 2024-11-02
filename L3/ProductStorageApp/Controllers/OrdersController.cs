cf79280923c7f39ded1752cd40f072735470f097
Grzegorz Sawicki <01178886@pw.edu.pl>
2024-11-02 20:41:21 +0100

    L3 - Orders - dodanie podstrony /Orders z listą zamówień i formularzem dodania nowego zamówienia - dodanie walidacji po stronie klienta do formularza dodania zamówienia. Formularz się nie wysyła, gdy chcemy stworzyć zamówienie Outgoing z większą ilością produktów, niż jest dostępnych - zmiana strony głównej /Home na stronę z wykresami (wcześniej /Chart) - zmiana ikon w panelu bocznym, usunięcie niepotrzebnego paska "Search.." w panelu górnym

---
+++ L3/ProductStorageApp/Controllers/OrdersController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductStorageApp.Utils;
+using ProductStorageApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Json;
+
+namespace ProductStorageApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly string ordersPath;
+        private readonly string productsPath;
+
+        private List<Product> products;
+        private List<Order> orders;
+
+        public OrdersController(IConfiguration configuration)
+        {
+            var dataPath = configuration["DataPath"];
+            ordersPath = Path.Combine(dataPath, "orders.json");
+            productsPath = Path.Combine(dataPath, "products.json");
+
+            products = Utils.Utils.getObjects<Product>(productsPath);
+            orders = Utils.Utils.getObjects<Order>(ordersPath);
+        }
+        public IActionResult Index()
+        {
+            orders = Utils.Utils.getObjects<Order>(ordersPath);
+            orders.Sort((a, b) => a.Date.CompareTo(b.Date));
+            orders.Reverse();
+
+            products = Utils.Utils.getObjects<Product>(productsPath);
+
+            var orderTypes = new SelectList(Enum.GetValues(typeof(OrderType)));
+
+            return View(new OrdersModel(orders, products, orderTypes));
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder(Order order)
+        {
+            //modyfikacja ilości danego produktu
+            int productIdToUpdate = order.ProductId;
+            Product productToUpdate = products.FirstOrDefault(p => p.Id == order.ProductId);
+            if (productToUpdate != null)
+            {
+                if (order.Type == OrderType.Incoming)
+                {
+                    productToUpdate.Quantity += order.ProductQuantity;
+                }
+                else if (order.Type == OrderType.Outgoing && productToUpdate.Quantity >= order.ProductQuantity)
+                {
+                    productToUpdate.Quantity -= order.ProductQuantity;
+                }
+                else //zamówiono więcej produktów niż aktualnie jest
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            var jsonString = System.IO.File.ReadAllText(ordersPath);
+            var objects = JsonSerializer.Deserialize<List<Order>>(jsonString) ?? new List<Order>();
+            order.Id = objects.Any() ? objects.Max(o => o.Id) + 1 : 1;
+            objects.Add(order);
+
+            Utils.Utils.writeObjects(products, productsPath);
+
+            Utils.Utils.writeObjects(objects, ordersPath);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- L3/ProductStorageApp/Models/ProductOrderModel.cs
+++ L3/ProductStorageApp/Models/ProductOrderModel.cs
@@ -4,16 +4,7 @@ namespace ProductStorageApp.Models
 {
     public class ProductOrderModel
     {
-        public Product Product { get; set; }
-        public Order Order { get; set; }
-
-        public ProductCategory ProductCategory { get; set; }
-        public SelectList ProductCategories { get; set; }
-
         public List<Product> Products { get; set; }
         public List<Order> Orders { get; set; }
-
-        public OrderType OrderType { get; set; }
-        public SelectList OrderTypes { get; set; }
     }
 }
